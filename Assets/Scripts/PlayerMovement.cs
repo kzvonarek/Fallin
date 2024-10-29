1ef5ffa022363a159a5ddbcf0b5c4336e0f5b350
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-10-28 17:30:56 -0700

    player movement
    touch and click controls, drags player w/ momentum

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Camera mainCamera;
+    [SerializeField] float speed = 5f; // speed to follow mouse/finger
+    [SerializeField] float maxSpeed = 10f; // max speed to follow mouse/finger
+    [SerializeField] float drag = 0.95f; // friction on mouse/finger release
+    private float horizVelocity;
+    private bool isDragging;
+    private float holdTime = 0f;
+    [SerializeField] float holdThreshold = 0.2f;
+
+    void Update()
+    {
+        isDragging = false;
+        Vector2 inputPosition = Vector2.zero;
+
+        // check if left mouse button is being held
+        if (Input.GetMouseButton(0))
+        {
+            isDragging = true;
+            inputPosition = Input.mousePosition;
+        }
+
+        // check if there is at least one finger holding screen
+        else if (Input.touchCount > 0)
+        {
+            isDragging = true;
+            inputPosition = Input.GetTouch(0).position;
+        }
+
+        if (isDragging)
+        {
+            holdTime += Time.deltaTime;
+
+            // make sure its a hold and not click/tap
+            if (holdTime >= holdThreshold)
+            {
+                // get mouse/finger position
+                Vector2 targetPosition = mainCamera.ScreenToWorldPoint(inputPosition);
+
+                // calculate velocity to follow the mouse/finger position
+                float targetVelocity = (targetPosition.x - transform.position.x) * speed;
+
+                // apply velocity to the player
+                horizVelocity = Mathf.Clamp(targetVelocity, -maxSpeed, maxSpeed);
+            }
+        }
+
+        else
+        {
+            holdTime = 0f;
+            horizVelocity *= drag;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // move player
+        transform.position = new Vector2(transform.position.x + horizVelocity * Time.deltaTime, transform.position.y);
+    }
+}
