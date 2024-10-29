b76ddf9eaef39ed4bd8e521324dcdcbd84d2ac3b
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-10-28 17:57:57 -0700

    floor behavior
    collisions with destroy zone (destroy the floor) and player ('game over'), new scripts, and object

---
+++ Assets/Scripts/FloorCollisions.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FloorCollisions : MonoBehaviour
+{
+    // check for floor collisions with other objects
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // end game on collision with player
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Game Over");
+        }
+
+        // destroy floor on collision with 'Destroy Zone'
+        if (other.gameObject.CompareTag("Destroy Zone"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
