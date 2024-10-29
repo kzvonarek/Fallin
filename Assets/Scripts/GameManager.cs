75a47c36ada3c7f0d3a1f655570554735ab8ec08
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2024-10-29 16:09:37 -0700

    score feature
    inc every 1 second

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,19 +8,28 @@ public class GameManager : MonoBehaviour
     [SerializeField] float objSpeedInc;
     [SerializeField] float maxObjSpeed;
 
-    // player original Y position for PortalBehavior.cs
-    [SerializeField] GameObject player;
-    public Rigidbody2D playerRb;
-    public float playerOrigYPos;
+    // score functionality
+    [SerializeField] TextMeshProUGUI inGameScoreText;
+    private int score;
+    private float scoreIncTimer = 0f;
 
     void Start()
     {
-        // get player original Y position [PortalBehavior.cs]
-        playerOrigYPos = playerRb.position.y;
+        Time.timeScale = 1;
+        score = 0;
     }
 
     void Update()
     {
+        // score increases by 1 every second alive
+        scoreIncTimer += Time.deltaTime;
+        if (scoreIncTimer >= 1f) // inc each second
+        {
+            score += 1;
+            inGameScoreText.text = score.ToString(); // update UI text
+            scoreIncTimer = 0f; // reset timer
+        }
+
         // floor speed increasing until it is greater than max floor speed [FloorMovement.cs]
         if (vertObjSpeed < maxObjSpeed)
         {
