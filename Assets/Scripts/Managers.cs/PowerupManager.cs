2af82334c287fc6baf096bb263499b9365206e4d
Kristijan Zvonarek <kristijanzvonarek24@gmail.com>
2025-01-09 20:27:15 -0500

    added bomb powerup

--- Assets/Scripts/Managers.cs/PowerupManager.cs
+++ Assets/Scripts/Managers.cs/PowerupManager.cs
@@ -9,6 +9,9 @@ public class PowerupManager : MonoBehaviour
     // mini powerup
     private bool currMini;
 
+    // bomb powerup
+    private bool destroyFloors;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -16,15 +19,29 @@ public class PowerupManager : MonoBehaviour
 
         // mini powerup
         currMini = false;
+
+        // bomb powerup
+        destroyFloors = false;
+
+    }
+
+    void Update()
+    {
+        // bomb powerup
+        if (destroyFloors)
+        {
+
+        }
     }
 
     private IEnumerator WaitAndRevert(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
+
+        // mini powerup
         if (currMini)
         {
             player.gameObject.transform.localScale = new Vector3(3.343594f, 3.343594f, 3.343594f);
-            Destroy(gameObject);
         }
     }
 
@@ -33,14 +50,37 @@ public class PowerupManager : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             uncollected = false;
+
+            // mini powerup
             if (this.gameObject.CompareTag("Mini Powerup"))
             {
                 other.gameObject.transform.localScale = new Vector3(1.671797f, 1.671797f, 1.671797f);
                 currMini = true;
+                Destroy(gameObject);
+
                 StartCoroutine(WaitAndRevert(6.0f));
+            }
+
+            // bomb powerup
+            if (this.gameObject.CompareTag("Bomb Powerup"))
+            {
+                Destroy(gameObject);
+                destroyFloors = true;
+
+                GameObject[] floors = GameObject.FindGameObjectsWithTag("Floor");
+                foreach (GameObject floor in floors)
+                {
+                    if (floor.transform.parent != null)
+                    {
+                        Destroy(floor.transform.parent.gameObject);
+                    }
+                    Destroy(floor);
+                }
+                destroyFloors = false;
             }
         }
 
+        // destroy powerup if uncollected
         if (other.gameObject.CompareTag("Destroy Zone") && uncollected)
         {
             Destroy(this.gameObject);
