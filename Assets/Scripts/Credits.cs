eab03cc90d5ed08ba54b05ced9de791ba313956f
Kristijan Zvonarek <kzvonare@usc.edu>
2025-07-01 01:23:46 -0400

    credits/final build

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,5 @@
+// Credits for Fallin'
+
+// OST: 'Best Game Console' by <a href="https://pixabay.com/users/djartmusic-46653586/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=301284">Krzysztof Szymanski</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=301284">Pixabay</a>
+// Background: 'Free Sky Backgrounds' by https://free-game-assets.itch.io/free-sky-with-clouds-background-pixel-art-set
+// Font: 'Retro Gaming' by https://www.dafont.com/retro-gaming.font
